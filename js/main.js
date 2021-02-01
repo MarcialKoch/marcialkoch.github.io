8eae556b190e304a5f895ffbb74fe680771e2d3c
MarcialKoch <37845027+MarcialKoch@users.noreply.github.com>
2021-02-01 21:21:28 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,87 @@
+var audio = ["sound/truecs/1.wav", "sound/truecs/2.wav", "sound/truecs/3.wav", "sound/truecs/4.wav", "sound/truecs/5.wav", "sound/truecs/6.wav", "sound/truecs/7.wav", "sound/truecs/8.wav", "sound/truecs/9.wav", "sound/truecs/10.wav", "sound/truecs/11.wav", "sound/truecs/12.wav", "sound/truecs/13.wav", "sound/truecs/14.wav"];
+
+$(document).ready(function(){
+  $('.category-container #topselection').addClass('textColor');
+  if ($(window).width() <= 400) {
+  if($("#german").is(':checked')){
+      $('.german-text').show();
+      $('.english-text').hide();
+      $('#germanbutton').addClass('underline');
+}  else{
+      $('.german-text').hide();
+      $('.english-text').show();
+    }
+  }
+});
+
+$('.category-container .category').click(function(){
+  var filtername = $(this).attr('id');
+  $('.category-container .category').removeClass('textColor');
+  $(this).addClass('textColor');
+if(filtername == "allprojects"){
+  $('.project-item-wrap').show();
+}
+else {
+  $('.project-item-wrap').show();
+  $('.project-item-wrap').not('[data-category*="'+ filtername +'"]').hide();
+}
+
+$('.text-category').text(filtername);
+  });
+
+
+$('.year-container .year').click(function(){
+  var filtername = $(this).attr('id');
+  $('.year-container .year').removeClass('textColor');
+  $(this).addClass('textColor');
+  if(filtername == "allprojects"){
+    $('.project-item-wrap').show();
+  }
+  else {
+    $('.project-item-wrap').show();
+    $('.project-item-wrap').not('[data-year*="'+ filtername +'"]').hide();
+  }
+  $('.text-category').text(filtername);
+    });
+
+
+  $(".project-item-wrap").hover(function(){
+    var categories = $(this).data("category");
+    var year = $(this).data("year");
+         $(categories).addClass('hover');
+         $("#" + year).addClass('hover');
+       },
+  function(){
+    var categories = $(this).data("category");
+    var year = $(this).data("year");
+    $(categories).removeClass('hover');
+    $("#" + year).removeClass('hover');
+  });
+  $('.img-description').each(function() {
+    $(this).css({"top":$(this).prev().height()/2});
+});
+if ($(window).width() <= 414) {
+$('#german').click(function() {
+    $('.german-text').show();
+    $('.english-text').hide();
+    $('#germanbutton').addClass('underline');
+    $('#englishbutton').removeClass('underline');
+});
+$('#english').click(function() {
+    $('.german-text').hide();
+    $('.english-text').show();
+    $('#englishbutton').addClass('underline');
+    $('#germanbutton').removeClass('underline');
+});
+}
+
+$('button.launchpad').click(function() {
+  var i = $(this).attr('id').substring(1);           //get the index of button
+  new Audio(audio[i - 1]).play();          //play corresponding audio
+});
+
+$(".about-picture").mouseover(function () {
+  $(this).attr('src', $(this).data("hover"));
+}).mouseout(function () {
+  $(this).attr('src', $(this).data("src"));
+});
