8da21989f4eb6ec5ba17de5fb5e7a523a353e085
MarcialKoch <37845027+MarcialKoch@users.noreply.github.com>
2023-07-27 12:30:37 +0200

    AR begin

--- testAR/sound.js
+++ testAR/sound.js
@@ -1,13 +1,10 @@
-<script>
-AFRAME.registerComponent('soundhandler', {
-    tick: function () {
-           var entity = document.querySelector('[sound]');
-         if (document.querySelector('a-marker').object3D.visible == true) {
-            entity.components.sound.playSound();
-        } else {
-            entity.components.sound.pauseSound();
-        }
+const audio = new Audio("assets/audio1.mp3");
+const buttons = document.querySelectorAll("button");
 
-     }
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    buttons.style.display = 'none';
+    audio.play();
+    alert("Hello! I am an alert box!!");
+  });
 });
-</script>
