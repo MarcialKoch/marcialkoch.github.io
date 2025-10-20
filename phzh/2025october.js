38fce15dd67f2d354a36d960e97a18e8cd710f2e
MarcialKoch <37845027+MarcialKoch@users.noreply.github.com>
2025-10-20 15:19:36 +0200

    Content PHZH

--- phzh/2025october.js
+++ phzh/2025october.js
@@ -1,14 +1,13 @@
 const texts = [
-  "First text",
-  "Second text",
-  "Third text",
-  "Fourth text",
-  "Fifth text",
-  "Sixth text",
-  "Seventh text",
-  "Eighth text",
-  "Ninth text",
-  "Tenth text"
+  "Wenn Sie sich etwas wünschen könnten, wie würde das ideale Klassenzimmer aussehen? ",
+  "Wenn Sie ein Algorithmus wären, welche Herausforderung im Bildungswesen würden Sie lösen?",
+  "Schauen Sie sich um: Was gefällt Ihnen am besten dort, wo Sie sich gerade befinden? Was fällt Ihnen auf?",
+  "Wenn Sie eine Lerntechnologie wären, was könnten Sie am besten?",
+  "Wenn Sie eine Fehlerkultur an Ihrer Schule einführen würden, was wäre dann Ihr Slogan?",
+  "Wie würden Sie gerne mit der PHZH zusammenarbeiten? Was wäre Ihr Traumprojekt?",
+  "Wie würden Sie gerne in Zukunft schreiben, allein oder mit KI?",
+  "Woran werden Lernende und Lehrpersonen in Zukunft erkennen, dass Lernen stattgefunden hat?",
+  "Wenn Sie noch einmal studieren könnten, was würden Sie dann wählen?"
 ];
 
 let i = 0;
